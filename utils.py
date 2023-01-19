0e312aa402f19c4dda09aafc2118c406eae5b84c
valosekj <jan.valosek@upol.cz>
2023-01-19 13:39:09 -0500

    Introduce fetch_subject_and_session function (to unify get_subject and get_session under a single function).

--- utils.py
+++ utils.py
@@ -18,22 +18,29 @@ import yaml
 
 
 # BIDS utility tool
-def get_subject(file):
+def fetch_subject_and_session(filename_path):
     """
-    Get subject from BIDS file name
-    :param file:
-    :return: subject
+    Get subject ID, session ID and filename from the input BIDS-compatible filename or file path
+    The function works both on absolute file path as well as filename
+    :param filename_path: input nifti filename (e.g., sub-001_ses-01_T1w.nii.gz) or file path
+    (e.g., /home/user/MRI/bids/derivatives/labels/sub-001/ses-01/anat/sub-001_ses-01_T1w.nii.gz
+    :return: subjectID: subject ID (e.g., sub-001)
+    :return: sessionID: session ID (e.g., ses-01)
+    :return: filename: nii filename (e.g., sub-001_ses-01_T1w.nii.gz)
     """
-    return file.split('_')[0]
 
+    _, filename = os.path.split(filename_path)              # Get just the filename (i.e., remove the path)
+    subject = re.search('sub-(.*?)[_/]', filename_path)
+    subjectID = subject.group(0)[:-1] if subject else ""    # [:-1] removes the last underscore or slash
+    session = re.findall(r'ses-..', filename_path)
+    sessionID = session[0] if session else ""               # Return None if there is no session
+    contrast = 'dwi' if 'dwi' in filename_path else 'anat'  # Return contrast (dwi or anat)
+    # REGEX explanation
+    # \d - digit
+    # \d? - no or one occurrence of digit
+    # *? - match the previous element as few times as possible (zero or more times)
 
-def get_contrast(file):
-    """
-    Get contrast from BIDS file name
-    :param file:
-    :return:
-    """
-    return 'dwi' if (file.split('_')[-1]).split('.')[0] == 'dwi' else 'anat'
+    return subjectID, sessionID, filename, contrast
 
 
 class SmartFormatter(argparse.HelpFormatter):
