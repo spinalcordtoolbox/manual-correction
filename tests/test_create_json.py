52d3a75a2a7fb4581aa686e06caae8869bfd5739
valosekj <jan.valosek@upol.cz>
2023-07-17 12:58:04 -0400

    Update tests for update_json function
    - test also appending

--- tests/test_create_json.py
+++ tests/test_create_json.py
@@ -1,6 +1,6 @@
 #######################################################################
 #
-# Test for create_json() function
+# Tests for update_json() function
 #
 # RUN BY:
 #   python -m pytest -v tests/test_create_json.py
@@ -8,12 +8,12 @@
 
 import time
 import json
-from manual_correction import create_json, check_if_modified
+from manual_correction import update_json
 
 
-def test_create_json_modified(tmp_path):
+def test_create_json(tmp_path):
     """
-    Test that the function create_json() creates a JSON file with the expected metadata if modified=True
+    Test that the function update_json() creates a JSON file with the expected metadata if modified=True.
     """
     # Create a temporary file for testing
     fname_label = "sub-001_ses-01_T1w_seg-manual.nii.gz"
@@ -21,10 +21,36 @@ def test_create_json_modified(tmp_path):
     nifti_file.touch()
 
     # Call the function with modified=True
-    create_json(str(nifti_file), "Test Rater", True)
+    update_json(str(nifti_file), "Test Rater", modified=True)
 
     # Check that the JSON file was created and contains the expected metadata
-    expected_metadata = {'Author': "Test Rater", 'Date': time.strftime('%Y-%m-%d %H:%M:%S')}
+    expected_metadata = {'GeneratedBy': [{'Author': "Test Rater", 'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
+    json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
+    assert json_file.exists()
+    with open(str(json_file), "r") as f:
+        metadata = json.load(f)
+    assert metadata == expected_metadata
+
+
+def test_update_json(tmp_path):
+    """
+    Test that the function update_json() updates (appends to) the JSON file with the expected metadata if modified=True.
+    """
+    # Create a temporary file for testing
+    fname_label = "sub-001_ses-01_T1w_seg-manual.nii.gz"
+    nifti_file = tmp_path / fname_label
+    nifti_file.touch()
+    # Create JSON file with some metadata
+    json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
+    with open(str(json_file), "w") as f:
+        json.dump({'GeneratedBy': [{'Author': "Test Rater 1", 'Date': "2023-01-01 00:00:00"}]}, f)
+
+    # Call the function with modified=True
+    update_json(str(nifti_file), "Test Rater 2", modified=True)
+
+    # Check that the JSON file was created and contains the expected metadata
+    expected_metadata = {'GeneratedBy': [{'Author': "Test Rater 1", 'Date': "2023-01-01 00:00:00"},
+                                         {'Author': "Test Rater 2", 'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
     json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
     assert json_file.exists()
     with open(str(json_file), "r") as f:
