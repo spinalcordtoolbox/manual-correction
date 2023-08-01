6c31ab2fc56343b7bbb6dbd62a8b891776037202
Nathan Molinier <nathan.molinier@gmail.com>
2023-08-01 16:35:25 -0400

    Add 'Y' option (do labeling for all files) to ask_if_modify()
    * Add 'Y' option (do labeling for all files) to ask_if_modify() function
    
    * Include do_labeling_always variable into tests
    
    * Add a new test for 'Y' option (do labeling for all files)
    
    ---------
    
    Co-authored-by: Jan Valosek <39456460+valosekj@users.noreply.github.com>
    Co-authored-by: valosekj <jan.valosek@upol.cz>

--- tests/test_ask_if_modify.py
+++ tests/test_ask_if_modify.py
@@ -33,12 +33,37 @@ def test_ask_if_modify_fname_label_exists_y(tmp_path, cleanup_files):
 
     # Patch input() function to simulate user input
     with patch('builtins.input', return_value='y'):
-        do_labeling, copy, create_empty_mask = ask_if_modify(fname_label, fname_seg)
+        do_labeling, copy, create_empty_mask, do_labeling_always = ask_if_modify(fname_label, fname_seg)
 
     # Check that the function returned the correct values
     assert do_labeling
     assert not copy
     assert not create_empty_mask
+    assert not do_labeling_always
+
+
+def test_ask_if_modify_fname_label_exists_Y(tmp_path, cleanup_files):
+    """
+    Test that the function ask_if_modify() returns the correct values when the label file exists and the user answers
+    "Y" (do labeling for all files)
+    """
+    # create some test files
+    path_data = os.path.join(tmp_path, "BIDS")
+    path_sub = os.path.join(path_data, "sub-001", "ses-01", "anat")
+    os.makedirs(path_sub, exist_ok=True)
+    fname_label = os.path.join(path_sub, "sub-001_ses-01_T1w_seg-manual.nii.gz")
+    fname_seg = os.path.join(path_sub, "sub-001_ses-01_T1w_seg.nii.gz")
+    open(fname_label, "w").close()
+
+    # Patch input() function to simulate user input
+    with patch('builtins.input', return_value='Y'):
+        do_labeling, copy, create_empty_mask, do_labeling_always = ask_if_modify(fname_label, fname_seg)
+
+    # Check that the function returned the correct values
+    assert do_labeling
+    assert not copy
+    assert not create_empty_mask
+    assert do_labeling_always
 
 
 def test_ask_if_modify_fname_label_exists_n(tmp_path, cleanup_files):
@@ -55,12 +80,13 @@ def test_ask_if_modify_fname_label_exists_n(tmp_path, cleanup_files):
 
     # Patch input() function to simulate user input
     with patch('builtins.input', return_value='n'):
-        do_labeling, copy, create_empty_mask = ask_if_modify(fname_label, fname_seg)
+        do_labeling, copy, create_empty_mask, do_labeling_always = ask_if_modify(fname_label, fname_seg)
 
     # Check that the function returned the correct values
     assert not do_labeling
     assert not copy
     assert not create_empty_mask
+    assert not do_labeling_always
 
 
 def test_ask_if_modify_fname_label_not_exist(tmp_path, cleanup_files):
@@ -76,12 +102,13 @@ def test_ask_if_modify_fname_label_not_exist(tmp_path, cleanup_files):
     open(fname_seg, "w").close()
 
     # Call function with non-existing fname_label
-    do_labeling, copy, create_empty_mask = ask_if_modify(fname_label, fname_seg)
+    do_labeling, copy, create_empty_mask, do_labeling_always = ask_if_modify(fname_label, fname_seg)
 
     # Check that the function returned the correct values
     assert do_labeling
     assert copy
     assert not create_empty_mask
+    assert not do_labeling_always
 
 
 def test_create_empty_mask(tmp_path, cleanup_files):
@@ -99,9 +126,10 @@ def test_create_empty_mask(tmp_path, cleanup_files):
     fname_seg = os.path.join(path_sub, "sub-001_ses-01_T1w_seg.nii.gz")
 
     # Call function with non-existing fname_label and fname_seg
-    do_labeling, copy, create_empty_mask = ask_if_modify(fname_label, fname_seg)
+    do_labeling, copy, create_empty_mask, do_labeling_always = ask_if_modify(fname_label, fname_seg)
 
     # Check that the function returned the correct values
     assert do_labeling
     assert not copy
     assert create_empty_mask
+    assert not do_labeling_always
