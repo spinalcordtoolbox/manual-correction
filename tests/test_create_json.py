481538195f8fe872d3ed2caab97079202e0ceac7
Jan Valosek <39456460+valosekj@users.noreply.github.com>
2024-02-14 14:54:29 -0500

    Add `"Name"` and `"SpatialReference"` key-value pair to JSON sidecar  (#76)
    * Add 'SpatialReference': 'orig' key-value pair based on https://github.com/spinalcordtoolbox/manual-correction/pull/76#issuecomment-1912384077
    
    * Change 'Author' to 'Name' based on https://github.com/spinalcordtoolbox/manual-correction/pull/76#issuecomment-1912384077. Move 'Name' to be first key.
    
    * Update tests with 'SpatialReference' and 'Name' keys
    
    * Remove the difference between modified and visually verified -- remove the `check_if_modified` function 
    
    ---------
    
    Co-authored-by: Nathan Molinier <nathan.molinier@polymtl.ca>

--- tests/test_create_json.py
+++ tests/test_create_json.py
@@ -13,7 +13,7 @@ from manual_correction import update_json
 
 def test_create_json(tmp_path):
     """
-    Test that the function update_json() creates a JSON file with the expected metadata if modified=True.
+    Test that the function update_json() creates a JSON file with the expected metadata
     """
     # Create a temporary file for testing
     fname_label = "sub-001_ses-01_T1w_seg-manual.nii.gz"
@@ -21,10 +21,13 @@ def test_create_json(tmp_path):
     nifti_file.touch()
 
     # Call the function with modified=True
-    update_json(str(nifti_file), "Test Rater", modified=True)
+    update_json(str(nifti_file), "Test Rater")
 
     # Check that the JSON file was created and contains the expected metadata
-    expected_metadata = {'GeneratedBy': [{'Author': "Test Rater", 'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
+    expected_metadata = {'SpatialReference': 'orig',
+                         'GeneratedBy': [{'Name': 'Manual',
+                                          'Author': "Test Rater",
+                                          'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
     json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
     assert json_file.exists()
     with open(str(json_file), "r") as f:
@@ -34,7 +37,7 @@ def test_create_json(tmp_path):
 
 def test_update_json(tmp_path):
     """
-    Test that the function update_json() updates (appends to) the JSON file with the expected metadata if modified=True.
+    Test that the function update_json() updates (appends to) the JSON file with the expected metadata.
     """
     # Create a temporary file for testing
     fname_label = "sub-001_ses-01_T1w_seg-manual.nii.gz"
@@ -43,14 +46,22 @@ def test_update_json(tmp_path):
     # Create JSON file with some metadata
     json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
     with open(str(json_file), "w") as f:
-        json.dump({'GeneratedBy': [{'Author': "Test Rater 1", 'Date': "2023-01-01 00:00:00"}]}, f)
+        json.dump({'SpatialReference': 'orig',
+                        'GeneratedBy': [{'Name': 'Manual',
+                                         'Author': "Test Rater 1",
+                                         'Date': "2023-01-01 00:00:00"}]}, f)
 
     # Call the function with modified=True
-    update_json(str(nifti_file), "Test Rater 2", modified=True)
+    update_json(str(nifti_file), "Test Rater 2")
 
     # Check that the JSON file was created and contains the expected metadata
-    expected_metadata = {'GeneratedBy': [{'Author': "Test Rater 1", 'Date': "2023-01-01 00:00:00"},
-                                         {'Author': "Test Rater 2", 'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
+    expected_metadata = {'SpatialReference': 'orig',
+                         'GeneratedBy': [{'Name': 'Manual',
+                                          'Author': "Test Rater 1",
+                                          'Date': "2023-01-01 00:00:00"},
+                                         {'Name': 'Manual',
+                                          'Author': "Test Rater 2",
+                                          'Date': time.strftime('%Y-%m-%d %H:%M:%S')}]}
     json_file = tmp_path / fname_label.replace(".nii.gz", ".json")
     assert json_file.exists()
     with open(str(json_file), "r") as f:
